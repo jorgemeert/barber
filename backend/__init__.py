c7ecf527cf53b172827e31ec3c91e5db636cd783
Jorge Augusto Flores Meert <jorgeflmrttt@icloud.com>
2026-06-09 18:56:51 -0300

    juntando models ao app
    juntando models ao app

---
+++ backend/__init__.py
@@ -0,0 +1,1 @@
+## Arquivo vázio, dentro da pasta backend apenas para dizer para o python que a pasta é um pacote.
--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,39 @@
+#Importação de bibliotecas.
 from flask import Flask
+from backend.extensions import db
+import os
+from dotenv import load_dotenv
 
+#Carregando env.
+load_dotenv()
+
+#Definição de váriavel com nome do app.
 app = Flask(__name__)
 
 
+#Configuração do banco de dados.
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') # lê o valorda váriavel SECRET_KEY que está .env.
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL') # É o endereço do banco de dados.
+
+
+#Inicialização do banco de dados.
+db.init_app(app)
+
+#Importação dos models, para o db precisa ver e quais tabelas ele irá criar.
+from backend.models.cliente import Cliente
+from backend.models.barbeiro import Barbeiro
+from backend.models.servico import Serviço
+from backend.models.agendamento import Agendamento
+from backend.models.bloqueio import Bloqueio
+
+
+#Inicialização do app.
 @app.route('/')
 def iniciar():
     return 'Está funcionado'
 
-
+# Verifica se o arquvio está sendo executado diretamente.
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context(): # Usado pois o SQLAlhemy precisa saber qual app está sendo usado no momento.
+        db.create_all() # Criaas tabelas no banco de dados.
+    app.run(debug=True)   #Reinicia o servidor automaticamente quando você salva o código.
