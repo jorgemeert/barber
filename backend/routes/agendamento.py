cf16c85f4c8119a8ff83de89865983b805377c68
Jorge Augusto Flores Meert <jorgeflmrttt@icloud.com>
2026-06-16 17:53:46 -0300

    backend, basicamente pronto
    partir para o Front agora

--- backend/routes/agendamento.py
+++ backend/routes/agendamento.py
@@ -28,7 +28,7 @@ def agendar_cliente():
     db.session.add(agendamento)
     db.session.commit()
 
-    return jsonify({'mensagem':f'Seu agendamento foi concluído pra o dia {agendamento.dia} no horário {agendamento.horario}'})
+    return jsonify({'mensagem':f'Seu agendamento foi concluído pra o dia {agendamento.dia} no horário {agendamento.horario}'}),200
 
 
 @agendamento_bp.route('/mostrarAgendamentos/<int:id_barbeiro>', methods = ['GET'])
@@ -37,7 +37,36 @@ def mostrar_agendamentos(id_barbeiro):
     agendamentos = Agendamento.query.filter_by(id_barbeiro = id_barbeiro).all()
 
     if not agendamentos:
-        return jsonify ({'mensagem':'Você não tem nenhum agendamento!'})
+        return jsonify ({'mensagem':'Você não tem nenhum agendamento!'}),404
 
 
-    return jsonify([{'dia': str(a.dia), 'horario': str(a.horario),'id_cliente': a.id_cliente,'id_servico': a.id_servico}for a in agendamentos])
+    return jsonify([{'dia': str(a.dia), 'horario': str(a.horario),'id_cliente': a.id_cliente,'id_servico': a.id_servico}for a in agendamentos]),200
+
+@agendamento_bp.route('/agendamentosAtivos/<int:id_cliente>', methods = ['GET'])
+def agendamentos_ativos(id_cliente):
+
+    agendamentos_cliente = Agendamento.query.filter_by(id_cliente = id_cliente).first()
+
+    if not agendamentos_cliente:
+        return jsonify({'mensagem':'Você não possui agendamentos!'}),404
+
+    return jsonify({
+        'dia': str(agendamentos_cliente.dia),
+        'horario': str(agendamentos_cliente.horario),
+        'id_barbeiro': agendamentos_cliente.id_barbeiro,
+        'id_servico': agendamentos_cliente.id_servico
+        }),200
+
+
+@agendamento_bp.route('/cancelarAgendamento/<int:id>', methods = ['DELETE'])
+def cancelar_agendamento(id):
+
+    id_agendamento = Agendamento.query.filter_by(id = id).first()
+
+    if not id_agendamento:
+        return jsonify({'mensagem':'Você não possui agendamentos'}),404
+
+    db.session.delete(id_agendamento)
+    db.session.commit()
+
+    return jsonify({'mensagem' : 'Agendamento cancelado com sucesso!'}),200
