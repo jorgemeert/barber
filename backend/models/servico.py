e9780a181268b70f804d910d6fd7f7988b152dbe
Jorge Augusto Flores Meert <jorgeflmrttt@icloud.com>
2026-07-09 18:30:17 -0300

    ulitmos ajustes para o deploy
    ulitmos ajustes para o deploy

--- backend/models/servico.py
+++ backend/models/servico.py
@@ -5,5 +5,5 @@ class Servico(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     nome_servico = db.Column(db.String(150),nullable = False)
-    valor = db.Column(db.Float(100),nullable = False)
+    valor = db.Column(db.Float, nullable = False)
     id_barbeiro = db.Column(db.Integer, db.ForeignKey('barbeiros.id'),nullable = False)
